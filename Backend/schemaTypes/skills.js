6d00d24a89aa2ae9525ce13b80d2cd76215706cd
Ankit <120703430+ankit3106@users.noreply.github.com>
2025-07-04 12:05:49 +0000

    skills page done

--- Backend/schemaTypes/skills.js
+++ Backend/schemaTypes/skills.js
@@ -1,26 +1,64 @@
-export default{
-    name:'skills',
-    title:'Skills',
+// export default{
+//     name:'skills',
+//     title:'Skills',
+//     type: 'document',
+//     fields:[
+//         {
+//             name:'name',
+//             title:'Name',
+//             type:'string'
+//         },
+//         {
+//             name:'bgColor',
+//             title:'BgColor',
+//             type:'string'
+//         },
+//         {
+//             name:'icon',
+//             title:'Icon',
+//             type: 'image',
+//             options: {
+//               hotspot: true,
+//             },
+//         },
+
+//     ]
+// }
+export default {
+    name: 'skills',
+    title: 'Skills',
     type: 'document',
-    fields:[
+    fields: [
         {
-            name:'name',
-            title:'Name',
-            type:'string'
+            name: 'name',
+            title: 'Name',
+            type: 'string'
         },
         {
-            name:'bgColor',
-            title:'BgColor',
-            type:'string'
+            name: 'bgColor',
+            title: 'BgColor',
+            type: 'string'
         },
         {
-            name:'icon',
-            title:'Icon',
+            name: 'icon',
+            title: 'Icon',
             type: 'image',
             options: {
-              hotspot: true,
+                hotspot: true,
             },
         },
-        
+        {
+            name: 'category',
+            title: 'Category',
+            type: 'string',
+            options: {
+                list: [
+                    { title: 'Languages', value: 'languages' },
+                    { title: 'Web Developer Skills', value: 'webdev' },
+                    { title: 'Web Developer Tools', value: 'devTools' },
+                ],
+                layout: 'radio', 
+            },
+        },
     ]
 }
